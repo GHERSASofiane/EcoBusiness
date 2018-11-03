aae79cfc8ed822734cd79acf21aa83df177aeb38
m.ghersa.s@gmail.com <m.ghersa.s@gmail.com>
2018-11-03 15:13:31 +0100

    Push And Dpeloyment GHERSA.

---
+++ Deployment.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -am 'Push And Dpeloyment GHERSA.'
+git push heroku master  
+git push -u origin master
--- herokuDeployment.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -am 'Heroku Dpeloyment.'
-git push heroku master  
-heroku open
