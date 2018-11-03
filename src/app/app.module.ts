550aa79432abd7bf8a63d93408e389f0c8ddb360
m.ghersa.s@gmail.com <m.ghersa.s@gmail.com>
2018-11-03 15:04:36 +0100

    Heroku Dpeloyment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,41 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { OfferSearchPage } from '../pages/offer-search/offer-search';
+import { OfferConsultPage } from '../pages/offer-consult/offer-consult';
+import { ProductProvider } from '../providers/product/product';
+import { EditOfferPage } from '../pages/edit-offer/edit-offer';
+import { ReservationRequestsPage } from '../pages/reservation-requests/reservation-requests';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    OfferSearchPage,
+    OfferConsultPage,
+    EditOfferPage,
+    ReservationRequestsPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    OfferSearchPage,
+    OfferConsultPage,
+    EditOfferPage,
+    ReservationRequestsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ProductProvider
   ]
 })
 export class AppModule {}
