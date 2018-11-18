dde0a22a7fe8da894bd082d92a95a21e67527024
m.ghersa.s@gmail.com <m.ghersa.s@gmail.com>
2018-11-19 00:34:48 +0100

    Push And Dpeloyment GHERSA. 19/11/2018

--- Deployment.sh
+++ Deployment.sh
@@ -1,5 +1,5 @@
 git pull
 git add .
-git commit -am 'Push And Dpeloyment GHERSA. 18/11/2018  '
+git commit -am 'Push And Dpeloyment GHERSA. 19/11/2018  '
 git push heroku master  
 git push -u origin master
